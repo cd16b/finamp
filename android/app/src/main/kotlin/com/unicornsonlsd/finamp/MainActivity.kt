f48ac32280ff94d7a199dae33efe28134aa2fd25
Chaphasilor <ppp.u@web.de>
2024-02-25 19:00:31 +0100

    Finamp can run on WearOS? 👀
    only some *very slight* overflow issues 😅

--- android/app/src/main/kotlin/com/unicornsonlsd/finamp/MainActivity.kt
+++ android/app/src/main/kotlin/com/unicornsonlsd/finamp/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.unicornsonlsd.finamp
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.annotation.NonNull
+import com.samsung.wearable_rotary.WearableRotaryPlugin
+import android.view.MotionEvent
 
 class MainActivity: FlutterActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // remove background to support round screens on wear os
+    intent.putExtra("background_mode", "transparent")
+    super.onCreate(savedInstanceState)
+  }
+
+  // handle rotational input device using wearable_rotary
+  override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
+    return when {
+      WearableRotaryPlugin.onGenericMotionEvent(event) -> true
+      else -> super.onGenericMotionEvent(event)
+    }
+  }
+  
 }
